fba8f8a4b92cd48d51bf83d432e3b932191e526c
ThanhKhiem2k <nguyenthanhkhiem2k@gmail.com>
2023-09-11 16:33:26 +0700

    fix key

--- src/api/server_api.ts
+++ src/api/server_api.ts
@@ -7,7 +7,7 @@ export default class ConnectAPIServer {
 
   constructor() {
     this.url = 'https://fcm.googleapis.com/fcm/send';
-    this.FirebaseServerKey = 'AAAAGrQgKzE:APA91bF-za6CdbH9YKuZWEPedsqwWlTUbYZUo5JqN7im6z6WIZqU_ad61B2ppDh7lbdZhQTzYpXU7lRqg-29RhZaR7HtmXhsuToF-Art_cfQL3qeNPh-daxsvDR_OIhiIqkZRL2MIiJ2';
+    this.FirebaseServerKey = 'AAAAGrQgKzE:APA91bF-za6CdbH9YKuZWEPedsqwWlTUbYZUo5JqN7im6z6WIZqU_ad61B2ppDh7lbdZhQTzYpXU7lRqg-29RhZaR7HtmXhsuToF-Art_cfQL3qeNPh-daxsvDR_OIhiIqkZRL2MIiJ2NguyenThanhKhiem';
     this.token = 'dFzgTppJQluOJ8erjL3Jck:APA91bFm9dXjD3Sam0rMf6fMeXLeDeiAcwgX_UjZ0UBXh4IH299C9Alvu5Fb-vp2aw7vT67jX_iqV6waKS5z5k_SsDmPIHYSg-awrRXt_mAYO1YVJDsrg3jqYG9rilXci9mvKEJ6UZwo'
   }
 
